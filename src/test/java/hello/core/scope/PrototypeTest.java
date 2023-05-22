240183a20660adf2c6af8e9eb051b358ad15b7c1
Hyunsoul37 <soulhyun37@gmail.com>
2023-05-23 01:02:10 +0900

    [TEST] Prototype Scope TestCode 작성
    - Prototype Bean은 스프링 컨테이너가 요청할때마다 생성된다.
    - 종료메서드가 실행되지 않는다.

---
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,46 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrototypeTest {
+
+    @Test
+    void prototypeTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        System.out.println("findPrototype Bean1");
+        PrototypeBean bean1 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("findPrototype Bean2");
+        PrototypeBean bean2 = ac.getBean(PrototypeBean.class);
+
+        System.out.println("bean1 = " + bean1);
+        System.out.println("bean2 = " + bean2);
+
+        assertThat(bean1).isNotSameAs(bean2);
+        ac.close();
+
+        // 생성된 bean의 destroy method 호출이 필요할경우 bean1.destroy() 수동으로 호출해야함
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
